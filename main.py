8606e772a19568ba22d8b25490ad55a24c28b792
Ahmed Hussein <wolktheline@yahoo.com>
2024-08-14 23:00:00 -0600

    Adding a footer to each page with corresponding topic names

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from fpdf import FPDF
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-
+pdf.set_auto_page_break(auto=False, margin=0)
 df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
     pdf.add_page()
@@ -10,7 +10,13 @@ for index, row in df.iterrows():
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=f"{row['Topic']}", align="L", ln=1, border=0)
     pdf.line(x1=10, y1=21, x2=200, y2=21)
+    pdf.ln(260)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.cell(w=0, h=12, txt=f"{row['Topic']}", align="R", ln=1, border=0)
     for i in range(row["Pages"]-1):
         pdf.add_page()
+        pdf.ln(260)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.cell(w=0, h=12, txt=f"{row['Topic']}", align="R", ln=1, border=0)
 
 pdf.output("output.pdf")
