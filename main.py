1e88671443de808f00804607f4ac865a527089dd
Ahmed Hussein <wolktheline@yahoo.com>
2024-08-14 23:24:35 -0600

    Adding lines to write.

--- main.py
+++ main.py
@@ -9,13 +9,16 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=f"{row['Topic']}", align="L", ln=1, border=0)
-    pdf.line(x1=10, y1=21, x2=200, y2=21)
-    pdf.ln(260)
+    for j in range(20, 300, 10):
+        pdf.line(x1=10, y1=j, x2=200, y2=j)
+    pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
     pdf.cell(w=0, h=12, txt=f"{row['Topic']}", align="R", ln=1, border=0)
-    for i in range(row["Pages"]-1):
+    for i in range(row["Pages"] - 1):
         pdf.add_page()
-        pdf.ln(260)
+        for j in range(10, 300, 10):
+            pdf.line(x1=10, y1=j, x2=200, y2=j)
+        pdf.ln(278)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.cell(w=0, h=12, txt=f"{row['Topic']}", align="R", ln=1, border=0)
 
